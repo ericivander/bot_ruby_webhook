476fa034da489b0a26778c02dad263a3f0713bdc
hajarbleh <johnstephanus@ymail.com>
2018-05-08 02:43:37 +0700

    fix typos

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 require 'sinatra/activerecord'
-require '../models/issue.rb'
-require '../models/user.rb'
+require_relative '../models/issue.rb'
+require_relative '../models/user.rb'
 
 User.create!(telegram_username: '@bukanjohn', telegram_user_id: 218550401, jira_user_key: 'johnstephanus', jira_user_email: 'johnstephanus@ymail.com')
