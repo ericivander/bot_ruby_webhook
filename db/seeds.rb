34085cf85b25fcebe12b60d3f5eabb5ecec754a7
Adi Faisal Rahman <adifaisal.r@gmail.com>
2018-05-08 11:49:59 +0700

    add user adi

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,4 @@ require_relative '../models/user.rb'
 
 User.create!(telegram_username: '@bukanjohn', telegram_user_id: 218550401, jira_user_key: 'johnstephanus', jira_user_email: 'johnstephanus@ymail.com')
 User.create!(telegram_username: '@putramuttaqin', telegram_user_id: 267031124, jira_user_key: 'putra.muttaqin', jira_user_email: 'putra.muttaqin@bukalapak.com')
+User.create!(telegram_username: '@adifaisalr', telegram_user_id: 463863778, jira_user_key: 'adi.faisal', jira_user_email: 'adi.faisal@bukalapak.com')
