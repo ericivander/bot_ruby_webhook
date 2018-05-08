cd54e9357158b38a407d0928232dc4d1e76b4092
ericivander <eric.ivander@bukalapak.com>
2018-05-08 19:10:56 +0700

    add seed eric and aranda

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,5 @@ User.create!(telegram_username: '@putramuttaqin', telegram_user_id: 267031124, j
 User.create!(telegram_username: '@adifaisalr', telegram_user_id: 463863778, jira_user_key: 'adi.faisal', jira_user_email: 'adi.faisal@bukalapak.com')
 User.create!(telegram_username: '@maulburz', telegram_user_id: 189841678, jira_user_key: 'maulana.muzakki', jira_user_email: 'maulana.muzakki@outlook.com')
 User.create!(telegram_username: '@windurisky', telegram_user_id: 376651180, jira_user_key: 'windu.risky', jira_user_email: 'windu.risky@bukalapak.com')
+User.create!(telegram_username: '@ericivander', telegram_user_id: 202215133, jira_user_key: 'eric.ivander', jira_user_email: 'eric.ivander@bukalapak.com')
+User.create!(telegram_username: '@arsoedjono', telegram_user_id: 290304974, jira_user_key: 'aranda.rizki', jira_user_email: 'aranda.rizki@bukalapak.com')
